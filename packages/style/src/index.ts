13d0555ca282882a67f7abe92422c6891ba808c4
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2025-09-07 16:03:16 -0500

    reexport VE bits

--- packages/style/src/index.ts
+++ packages/style/src/index.ts
@@ -1,12 +1,16 @@
-// Theme contract and implementations
-export { themeContract } from './theme-contract.css'
-export { lightTheme, darkTheme } from './themes.css'
+// import and reexport VE bits (we'll manage all VE bits here in style)
+export { recipe, type RecipeVariants } from '@vanilla-extract/recipes'
+export { globalStyle } from '@vanilla-extract/css'
 
 // Global styles
 import './global-styles.css'
 
 // Fonts
 import './fonts.css'
+
+// Theme contract and implementations
+export { themeContract } from './theme-contract.css'
+export { lightTheme, darkTheme } from './themes.css'
 
 // Theme provider and hook
 export { ThemeProvider, ThemeContext } from './theme-provider'
