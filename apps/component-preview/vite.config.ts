d29eca8478bfcca20cab7b6881824473b7e3d14d
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2025-09-08 11:12:59 -0500

    add shared package and clean up typechecking scripts

--- apps/component-preview/vite.config.ts
+++ apps/component-preview/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import path from 'path';
+
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
