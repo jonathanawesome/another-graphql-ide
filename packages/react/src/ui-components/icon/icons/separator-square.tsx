3d9ed33ac3ecbd5c9f65aee59872d925a5cd4331
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2025-09-08 10:02:34 -0500

    add icons

---
+++ packages/react/src/ui-components/icon/icons/separator-square.tsx
@@ -0,0 +1,11 @@
+export const SeparatorSquare = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="6.25" y="6.25" width="3.5" height="3.5" fill="#F7F7F7" />
+  </svg>
+)
