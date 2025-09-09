9fb3e386960710ab089decec15282e12a104fef4
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2025-09-09 13:45:53 -0500

    additional test schema

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export { createPreview } from './component-preview/create-preview'
 
 export { enterpriseTestSchema } from './test-schemas/enterprise-test-schema'
 export { graphiqlTestSchema } from './test-schemas/graphiql-test-schema'
+export { veterinaryOfficeSchema } from './test-schemas/veterinary-office-schema'
