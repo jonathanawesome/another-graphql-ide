53124385d7ce433991ebf3acf203cc8ade83d93b
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2025-09-09 09:39:02 -0500

    DRY routes

--- apps/component-preview/src/routes/$componentId.demo.$demoName.tsx
+++ apps/component-preview/src/routes/$componentId.demo.$demoName.tsx
@@ -1,46 +1,22 @@
-/* eslint-disable @typescript-eslint/only-throw-error */
-import { createFileRoute, redirect } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import { createFileRoute } from '@tanstack/react-router'
 
 import { Preview } from '../components/preview'
-import { discoverComponents } from '../utils/discovery'
+import { createPreviewLoader, usePreviewComponent } from '../utils/route-utils'
 
 export const Route = createFileRoute('/$componentId/demo/$demoName')({
-  loader: ({ params }) => {
-    const components = discoverComponents()
-    const component = components.find(c => c.id === params.componentId)
-
-    if (!component) {
-      throw redirect({ to: '/' })
-    }
-
-    if (!component.demos) {
-      throw redirect({ to: '/' })
-    }
-
-    const demo = component.demos.find(d => d.name === params.demoName)
-    if (!demo) {
-      throw redirect({ to: '/' })
-    }
-
-    return { component, demo }
-  },
+  loader: createPreviewLoader('demo'),
   component: DemoRoute,
 })
 
 function DemoRoute() {
   const { componentId, demoName } = Route.useParams()
-  const components = useMemo(() => discoverComponents(), [])
-  const selectedComponent = components.find(c => c.id === componentId)
-  const preview = selectedComponent ? {
-    title: selectedComponent.title,
-    component: selectedComponent.component,
-    category: selectedComponent.category,
-    variants: selectedComponent.variants,
-    demos: selectedComponent.demos
-  } : null
+  const preview = usePreviewComponent(componentId)
 
   return (
-    <Preview preview={preview} selectedItem={demoName} selectedType="demo" />
+    <Preview
+      preview={preview}
+      selectedItem={demoName || null}
+      selectedType="demo"
+    />
   )
 }
--- apps/component-preview/src/routes/$componentId.variant.$variantName.tsx
+++ apps/component-preview/src/routes/$componentId.variant.$variantName.tsx
@@ -1,49 +1,21 @@
-/* eslint-disable @typescript-eslint/only-throw-error */
-import { createFileRoute, redirect } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import { createFileRoute } from '@tanstack/react-router'
 
 import { Preview } from '../components/preview'
-import { discoverComponents } from '../utils/discovery'
+import { createPreviewLoader, usePreviewComponent } from '../utils/route-utils'
 
 export const Route = createFileRoute('/$componentId/variant/$variantName')({
-  loader: ({ params }) => {
-    const components = discoverComponents()
-    const component = components.find(c => c.id === params.componentId)
-
-    if (!component) {
-      throw redirect({ to: '/' })
-    }
-
-    if (!component.variants) {
-      throw redirect({ to: '/' })
-    }
-
-    const variant = component.variants.find(v => v.name === params.variantName)
-    if (!variant) {
-      throw redirect({ to: '/' })
-    }
-
-    return { component, variant }
-  },
+  loader: createPreviewLoader('variant'),
   component: VariantRoute,
 })
 
 function VariantRoute() {
   const { componentId, variantName } = Route.useParams()
-  const components = useMemo(() => discoverComponents(), [])
-  const selectedComponent = components.find(c => c.id === componentId)
-  const preview = selectedComponent ? {
-    title: selectedComponent.title,
-    component: selectedComponent.component,
-    category: selectedComponent.category,
-    variants: selectedComponent.variants,
-    demos: selectedComponent.demos
-  } : null
+  const preview = usePreviewComponent(componentId)
 
   return (
     <Preview
       preview={preview}
-      selectedItem={variantName}
+      selectedItem={variantName || null}
       selectedType="variant"
     />
   )
